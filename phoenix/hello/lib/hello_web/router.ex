43f825d2670d827ad913c1be384b1cd564700405
quentinhacourt <quentin.hacourt@student.ucll.be>
2020-05-04 23:46:29 +0200

    ecto

--- phoenix/hello/lib/hello_web/router.ex
+++ phoenix/hello/lib/hello_web/router.ex
@@ -19,6 +19,11 @@ defmodule HelloWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+
+    resources "/sessions", SessionController,
+      only: [:new, :create, :delete],
+      singleton: true
+
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
     resources "/reviews", ReviewController
@@ -29,22 +34,40 @@ defmodule HelloWeb.Router do
     resources "/posts", PostController
   end
 
-
   scope "/admin", HelloWeb.Admin, as: :admin do
     pipe_through :browser
 
-    resources "/images",  ImageController
+    resources "/images", ImageController
     resources "/reviews", ReviewController
-    resources "/users",   UserController
+    resources "/users", UserController
   end
 
   scope "/api", HelloWeb.Api, as: :api do
     pipe_through :api
 
     scope "/v1", V1, as: :v1 do
-      resources "/images",  ImageController
+      resources "/images", ImageController
       resources "/reviews", ReviewController
-      resources "/users",   UserController
+      resources "/users", UserController
+    end
+  end
+
+  scope "/cms", HelloWeb.CMS, as: :cms do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/pages", PageController
+  end
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+
+      user_id ->
+        assign(conn, :current_user, Hello.Accounts.get_user!(user_id))
     end
   end
 end
